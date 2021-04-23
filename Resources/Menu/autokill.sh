bd0bc3b99db374e21a1c7c3a82c689061f44cff8
Dopekid <joashsingh14@gmail.com>
2021-04-23 03:39:39 +0200

    Update autokill.sh

--- Resources/Menu/autokill.sh
+++ Resources/Menu/autokill.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
-# Script by : LostServer
+
 clear
 echo -e ""
 echo -e "======================================"
-echo -e ""
-echo -e "     AutoScriptVPS by LostServer      "
-echo -e ""
-echo -e "     [1]  AutoKill After 5 Minutes"
-echo -e "     [2]  AutoKill After 15 Minutes"
-echo -e "     [3]  AutoKill After 30 Minutes"
+echo -e "     [1]  AutoKill After 1 Minutes    "
+echo -e "     [2]  AutoKill After 15 Minutes   "
+echo -e "     [3]  AutoKill After 30 Minutes   "
 echo -e "     [4]  Turn Off AutoKill/MultiLogin"
-echo -e "     [x]  Exit"
+echo -e "     [x]  Exit                        "
 echo -e "======================================"                                                                                                          
 echo -e ""
 read -p "     Select From Options [1-4 or x] :  " AutoKill
@@ -21,14 +18,11 @@ case $AutoKill in
                 sleep 1
                 clear
                 rm -f /etc/cron.d/tendang
-                echo "*/5 * * * *  root /usr/bin/tendang" > /etc/cron.d/tendang
+                echo "*/1 * * * *  root /usr/bin/tendang" > /etc/cron.d/tendang
                 echo -e ""
                 echo -e "======================================"
-                echo -e "      AutoScriptVPS by LostServer     "
-                echo -e ""
-                echo -e "      Allowed MultiLogin : 2"
-                echo -e "      AutoKill Every     : 5 Minutes"      
-                echo -e ""
+                echo -e "      Allowed MultiLogin : 2          "
+                echo -e "      AutoKill Every     : 1 Minutes  "     
                 echo -e "======================================"                                                                                                                                 
                 exit                                                                  
                 ;;
@@ -40,12 +34,8 @@ case $AutoKill in
                 echo "*/5 * * * *  root /usr/bin/tendang" > /etc/cron.d/tendang
                 echo -e ""
                 echo -e "======================================"
-                echo -e ""
-                echo -e "      AutoScriptVPS by LostServer     "
-                echo -e ""
-                echo -e "      Allowed MultiLogin : 2"
-                echo -e "      AutoKill Every     : 15 Minutes"
-                echo -e ""
+                echo -e "      Allowed MultiLogin : 2          "
+                echo -e "      AutoKill Every     : 15 Minutes "
                 echo -e "======================================"
                 exit
                 ;;
@@ -57,25 +47,16 @@ case $AutoKill in
                 echo "*/5 * * * *  root /usr/bin/tendang" > /etc/cron.d/tendang
                 echo -e ""
                 echo -e "======================================"
-                echo -e ""
-                echo -e "      AutoScriptVPS by LostServer     "
-                echo -e ""
-                echo -e "      Allowed MultiLogin : 2"
-                echo -e "      AutoKill Every     : 30 Minutes"
-                echo -e ""
+                echo -e "      Allowed MultiLogin : 2          "
+                echo -e "      AutoKill Every     : 30 Minutes "
                 echo -e "======================================"
-                exit
                 ;;
                 4)
                 clear
                 rm -f /etc/cron.d/tendang
                 echo -e ""
                 echo -e "======================================"
-                echo -e ""
-                echo -e "      AutoScriptVPS by LostServer     "
-                echo -e ""
                 echo -e "      AutoKill MultiLogin Turned Off  "
-                echo -e ""
                 echo -e "======================================"
                 exit
                 ;;
