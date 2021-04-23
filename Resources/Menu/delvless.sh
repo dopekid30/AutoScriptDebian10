230418e7ed71512ec807ed3f9daa6b7aef109de5
Dopekid <joashsingh14@gmail.com>
2021-04-23 06:09:36 +0200

    Update delvless.sh

--- Resources/Menu/delvless.sh
+++ Resources/Menu/delvless.sh
@@ -8,9 +8,9 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^### " "/etc/v2ray/vless.json")
 
 	clear
 	echo ""
-	echo " Select the existing client you want to remove"
-	echo " Press CTRL+C to return"
-	echo " ==============================="
+        echo " ===================================="
+	echo " Select The Client You Want To Remove"
+	echo " ===================================="
 	echo "     No  Expired   User"
 	grep -E "^### " "/etc/v2ray/vless.json" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
@@ -27,10 +27,11 @@ sed -i "/^### $user $exp/,/^},{/d" /etc/v2ray/vnone.json
 systemctl restart v2ray@vless
 systemctl restart v2ray@none
 clear
-echo " V2RAY Account Deleted Successfully"
-echo " =========================="
+echo " ==========================="
+echo " Account Deleted Successfull"
+echo " ==========================="
 echo " Client Name : $user"
 echo " Expired On  : $exp"
-echo " =========================="
+echo " ==========================="
 echo -e "By LostServer"
 
